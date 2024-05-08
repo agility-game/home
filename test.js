1dc24cf7fd12b3f9a5bb403665cf23a91f5051ec
Willem van Heemstra <wvanheemstra@icloud.com>
2024-05-08 08:48:58 +0000

    Update

--- test.js
+++ test.js
@@ -26,5 +26,22 @@ describe('files', function () {
         });
     });
 	
-	// [..]
+    describe('import', function () {
+        it('should import pdf', function () {
+            assert.isTrue(true);
+        });
+
+        it('should import html', function () {
+            assert.isTrue(true);
+        });
+
+        it('should import yml', function () {
+            assert.isTrue(true);
+        });
+
+        it('should import text', function () {
+            assert.isTrue(true);
+        });
+    });
+
 });
